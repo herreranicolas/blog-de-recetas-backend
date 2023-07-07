55118f23c2f00d3690ca7d09ddca7b363e41b26e
Nicolas Herrera <herreranicoo17@gmail.com>
2023-07-07 20:26:19 -0300

    Agregue middlewares cors, json, morgan y static

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
 
 const app = express();
+
+// Puerto
 
 app.set("port", process.env.PORT || 4000);
 
 app.listen(app.get("port"), () => {
   console.log("Estoy en el puerto " + app.get("port"));
 });
+
+// Middlewares
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "/public")));
