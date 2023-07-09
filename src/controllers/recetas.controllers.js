5fa4347e3a52788664d104184fa2c6a71ad07670
Nicolas Herrera <herreranicoo17@gmail.com>
2023-07-09 12:16:49 -0300

    Agregue la carpeta helpers, con los archivos validacionReceta y resultadoValidacion

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -27,12 +27,6 @@ export const obtenerRecetaPorId = async (req, res) => {
 
 export const crearReceta = async (req, res) => {
   try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errores: errors.array(),
-      });
-    }
     const nuevaReceta = new Receta(req.body);
     await nuevaReceta.save();
     res.status(201).json({
--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -6,48 +6,16 @@ import {
   obtenerRecetaPorId,
   obtenerRecetas,
 } from "../controllers/recetas.controllers";
-import { check } from "express-validator";
+import validarReceta from "../helpers/validacionReceta";
 
 const router = Router();
 
-router
-  .route("/recetas")
-  .get(obtenerRecetas)
-  .post(
-    [
-      check("nombre")
-        .notEmpty()
-        .withMessage("El nombre de la receta es obligatorio")
-        .isLength({ min: 3, max: 50 })
-        .withMessage(
-          "El nombre de la receta debe tener entre 3 y 50 caracteres como máximo."
-        ),
-      check("descripcion")
-        .notEmpty()
-        .withMessage("La descripcion de la receta es obligatoria")
-        .isLength({ min: 3, max: 200 })
-        .withMessage(
-          "La descripcion de la receta debe tener entre 3 y 200 caracteres como máximo."
-        ),
-      check("ingredientes")
-        .notEmpty()
-        .withMessage("Los ingredientes de la receta son obligatorios."),
-      check("pasos")
-        .notEmpty()
-        .withMessage("Los pasos de la receta son obligatorios."),
-      check("imagen")
-        .notEmpty()
-        .withMessage("La direccion URL de la imagen es obligatoria")
-        .matches(/^https?:\/\/\S+\.(jpg|jpeg|png|gif)$/i)
-        .withMessage("La direccion de la imagen ingresada no es válida."),
-    ],
-    crearReceta
-  );
+router.route("/recetas").get(obtenerRecetas).post(validarReceta, crearReceta);
 
 router
   .route("/recetas/:id")
   .get(obtenerRecetaPorId)
   .delete(borrarReceta)
-  .put(editarReceta);
+  .put(validarReceta, editarReceta);
 
 export default router;
