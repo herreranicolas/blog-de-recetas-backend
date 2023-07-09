8dad16a134acdab69630a19a289d0cbbbdf1c191
Nicolas Herrera <herreranicoo17@gmail.com>
2023-07-09 11:51:27 -0300

    Agregue validaciones a los campos descripcion, ingredientes, pasos e imagen

--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -20,8 +20,26 @@ router
         .withMessage("El nombre de la receta es obligatorio")
         .isLength({ min: 3, max: 50 })
         .withMessage(
-          "El nombre de la receta debe tener entre 3 y 50 caracteres como máximo"
+          "El nombre de la receta debe tener entre 3 y 50 caracteres como máximo."
         ),
+      check("descripcion")
+        .notEmpty()
+        .withMessage("La descripcion de la receta es obligatoria")
+        .isLength({ min: 3, max: 200 })
+        .withMessage(
+          "La descripcion de la receta debe tener entre 3 y 200 caracteres como máximo."
+        ),
+      check("ingredientes")
+        .notEmpty()
+        .withMessage("Los ingredientes de la receta son obligatorios."),
+      check("pasos")
+        .notEmpty()
+        .withMessage("Los pasos de la receta son obligatorios."),
+      check("imagen")
+        .notEmpty()
+        .withMessage("La direccion URL de la imagen es obligatoria")
+        .matches(/^https?:\/\/\S+\.(jpg|jpeg|png|gif)$/i)
+        .withMessage("La direccion de la imagen ingresada no es válida."),
     ],
     crearReceta
   );
